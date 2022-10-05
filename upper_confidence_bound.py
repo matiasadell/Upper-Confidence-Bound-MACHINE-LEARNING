47cd9dbb458f41b0bff97185e100c0886e649f60
Matias Adell <80477474+matiasadell@users.noreply.github.com>
2022-10-04 23:00:55 -0300

    Update upper_confidence_bound.py

--- upper_confidence_bound.py
+++ upper_confidence_bound.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr  7 12:41:01 2019
-
-@author: juangabriel
-"""
-
 # Upper Confidence Bound (UCB)
 
 # Importar las librerías
@@ -24,19 +16,19 @@ number_of_selections = [0] * d      # Cantidad de seleccion de anuncios, 0 * n s
 sums_of_rewards = [0] * d           # Cantidad de clicks que tuvieron los anuncios
 ads_selected = []
 total_reward = 0
-for n in range(0, N):       # Cantidad de rondas de los usuarios
+for n in range(0, N):      
     max_upper_bound = 0     # Anuncio mas clickeado
     ad = 0
-    for i in range(0, d):       # Cantidad de rondas de los anuncios
-        if(number_of_selections[i]>0):      # Hacemos esto para que las primeras 10 rondas es para recaber informacion y las demas para sacar
-            average_reward = sums_of_rewards[i] / number_of_selections[i]       # La recompensa media del anuncio i hasta la ronda n
-            delta_i = math.sqrt(3/2*math.log(n+1)/number_of_selections[i])      # Formula del intervalo de confianza
-            upper_bound = average_reward + delta_i          # Formula del intervalo de confianza
+    for i in range(0, d):    
+        if(number_of_selections[i]>0):     
+            average_reward = sums_of_rewards[i] / number_of_selections[i]       
+            delta_i = math.sqrt(3/2*math.log(n+1)/number_of_selections[i])     
+            upper_bound = average_reward + delta_i         
         else:
             upper_bound = 1e400
             
         if upper_bound > max_upper_bound:      
-            max_upper_bound = upper_bound        # Conseguimos el anuncio mas clickeado
+            max_upper_bound = upper_bound        
             ad = i
     ads_selected.append(ad)
     number_of_selections[ad] = number_of_selections[ad] + 1
